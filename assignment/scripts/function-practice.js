f997508783db071276e290bb4b946b77af94f757
Reese Avery Kling <raveryk@gmail.com>
2021-03-21 14:32:07 -0500

    Q10 Work

--- assignment/scripts/function-practice.js
+++ assignment/scripts/function-practice.js
@@ -14,11 +14,12 @@ console.log('Test - should say "Hello World!"', hello());
 
 // 2. Function to return an personalized hello, for example 'Hello, Jo!'
 function helloName( name ) {
-  return 'Greetings, ' + name;
+  return `Greetings, ${name}!`;
 } // end helloName
 
 // Remember to call the function to test
 console.log(helloName('Reese') );
+console.log(helloName('BOB') );
 
 // 3. Function to add two numbers together & return the result
 function addNumbers( firstNumber, secondNumber ) {
@@ -51,6 +52,7 @@ function isPositive( number ) {
     return false;
 }
 }
+
 // Call the function to test each outcome (true & false)
 // Write a separate console.log statement for each outcome
 console.log( 'isPositive - should say true', isPositive(3) );
@@ -68,9 +70,9 @@ function getLast( array ) {
   }
 }
 
-console.log(getLast([1, 2, 3, 4]));
-console.log(getLast(['yes', 'no', 'maybe', "I don't know", 'most definitely']));
-console.log(getLast([]));
+console.log('Should say 4', getLast([1, 2, 3, 4]));
+console.log('Should ask: Can you repeat the question?', getLast(['yes', 'no', 'maybe', "I don't know", 'Can you repeat the question?']) );
+console.log('Should say undefined:', getLast([]));
 
 // 7. Function to find a value in an array. Return true if the
 //    value is found and false otherwise. Use a loop;
@@ -80,12 +82,13 @@ function find( value, array ){
     if ( item === value ) {
       return true;
     }
-  } return false; /// Definitely need to ask question about this!! Why did it not work when I used an if else statement but did work when my return false statement was outside the scope of the loop?? Or does that have to do with the 'for of' loop?
+  } return false; /// Definitely need to ask question about this!! Why did it not work when I used an if else statement but did work when my 'return false' statement was outside the scope of the if statement?? Or does that have to do with the 'for of' loop?
 } // end find
 
 console.log(find( 7, [1, 3, 5, 7]));
 console.log(find( 2, [2, 4, 3, 8]));
-console.log(find( 3, [9, 6, 3]));
+console.log(find( 3, [9, 6, 3, 4]));
+console.log(find( 6, [9, 6, 3, 4]));
 console.log(find( 4, [1, 2, 3, 5]));
 // ----------------------
 // Stretch Goals
@@ -109,18 +112,40 @@ function sumAll( array ) {
   // TODO: loop to add items
   while ( i < array.length ) {
     sum += array[i++];
-  }
-  return sum;
-} // end sumALl
+}
+    return sum;
+} // end sumAll
 
 console.log( sumAll( [1, 3, 5, 7]));
 console.log( sumAll( [6, 3, 4, 6, 10]));
 console.log( sumAll( [3, 5]));
 
 
+
 // 10. Function to return a new array of all positive (greater than zero)
 //     numbers contained in an input array. If there are no positive numbers
 //     return an empty array. Note: The input array should not change.
+
+const array1 = [ 0, 1, 2, 3, -4, -5, 6 ];
+let newArray = [ ];
+
+//
+function positiveNums ( array ) { //Creating function positiveNums with an input argument of an array.
+  for( let i = 0; i < array1.length; i++ ) { //For loop loops through the array.
+    if ( array1[i] > 0 ) { //If a number at any index within the array is greater than zero it will then be pushed into newArray.
+      newArray.push(array1[i]);
+    }
+  }
+}
+
+positiveNums(array1); //Calling the function and passing array1 through it.
+
+console.log(newArray); //Testing to see what has ended up in newArray.
+console.log(array1); //Testing to see if array1 is still the same.
+
+// let newArray = array1.filter(function (i) {
+//     return array1[i] > 0;
+// });
 
 
 
